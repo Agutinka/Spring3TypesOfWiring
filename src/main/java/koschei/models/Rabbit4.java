295dbaade84cdc0ce9b41a68f931e994fd45e8da
Agutinka <alenaagutina24@gmail.com>
2023-12-19 16:51:04 +0300

    Виды связывания бинов. Ответ 'На свете есть океан , на океане остров , на острове дерево , на дереве заяц , в зайце утка , в утке яйцо , в яйце иголка , смерть Кощея на игле :( '

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -2,11 +2,17 @@ package koschei.models;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("rabbit4")
 public class Rabbit4 {
+
+    private final Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
